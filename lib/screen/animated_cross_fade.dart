3742ecd85c776d898a22b03f9614da3af08e3db5
KhizarJamshaidIqbal <112169672+KhizarJamshaidIqbal@users.noreply.github.com>
2022-10-20 10:49:58 -0700

    change

--- lib/screen/animated_cross_fade.dart
+++ lib/screen/animated_cross_fade.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: camel_case_types, implementation_imports, prefer_const_constructors, unnecessary_import
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,10 +14,42 @@ class animated_cross_fade extends StatefulWidget {
 }
 
 class _animated_cross_fadeState extends State<animated_cross_fade> {
+  bool isFirst = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      reload();
+    });
+  }
+
+  void reload() {
+    setState(() {
+      isFirst = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: AnimatedCrossFade(
+          duration: Duration(seconds: 2),
+          firstChild: Container(
+            height: 200,
+            width: 200,
+            color: Colors.blueGrey,
+          ),
+          secondChild: Image(
+            image: AssetImage("Images/flutter.png"),
+            height: 200,
+            width: 200,
+          ),
+          crossFadeState:
+              isFirst ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+        ),
+      ),
     );
   }
 }
